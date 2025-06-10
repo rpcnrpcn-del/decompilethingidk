25cfbcc6d43edf60ca0e09d114e70d2a267a5da6
niko <nikcolefliko@gmail.com>
2025-06-10 18:22:10 +0200

    fix debug screen mode

--- Assets/Scripts/Assembly-CSharp/ControllerIO.cs
+++ Assets/Scripts/Assembly-CSharp/ControllerIO.cs
@@ -364,7 +364,7 @@ public class ControllerIO : MonoBehaviour
 			{
 				return InputMode.SteamVR_Oculus;
 			}
-			if (SessionManager.IsDeveloper && Application.isEditor && !VRDevice.isPresent)
+			if (Application.isEditor && !VRDevice.isPresent)
 			{
 				return InputMode.Debug;
 			}
